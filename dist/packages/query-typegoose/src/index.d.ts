80eaee3bad7e0a63e0c8da5e726e7a47638b1e27
Ruud <r.vanbuul@bedandbreakfast.eu>
2023-07-01 14:45:26 -0600

    fix: add global cache module

--- dist/packages/query-typegoose/src/index.d.ts
+++ dist/packages/query-typegoose/src/index.d.ts
@@ -1,3 +1,4 @@
+export { NestjsQueryTypegooseCacheModule } from './cache-module';
 export * from './decorators/relation-cache.decorator';
 export { NestjsQueryTypegooseModule } from './module';
 export { TypegooseQueryService, TypegooseQueryServiceOpts } from './services';
--- dist/packages/query-typegoose/src/index.js
+++ dist/packages/query-typegoose/src/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TypegooseQueryService = exports.NestjsQueryTypegooseModule = void 0;
+exports.TypegooseQueryService = exports.NestjsQueryTypegooseModule = exports.NestjsQueryTypegooseCacheModule = void 0;
 const tslib_1 = require("tslib");
+var cache_module_1 = require("./cache-module");
+Object.defineProperty(exports, "NestjsQueryTypegooseCacheModule", { enumerable: true, get: function () { return cache_module_1.NestjsQueryTypegooseCacheModule; } });
 tslib_1.__exportStar(require("./decorators/relation-cache.decorator"), exports);
 var module_1 = require("./module");
 Object.defineProperty(exports, "NestjsQueryTypegooseModule", { enumerable: true, get: function () { return module_1.NestjsQueryTypegooseModule; } });
