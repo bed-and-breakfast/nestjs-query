7f9110c52bd84033c98484d2fb635a001fca7738
Ruud <r.vanbuul@bedandbreakfast.eu>
2023-09-25 11:39:59 -0600

    fix: increase cache mem limit

--- packages/query-typegoose/src/cache-module.ts
+++ packages/query-typegoose/src/cache-module.ts
@@ -5,7 +5,7 @@ import { ReferenceCacheService } from './services/reference-cache.service'
 
 @Global()
 @Module({
-  imports: [CacheModule.register({ ttl: 0, max: 100000 })],
+  imports: [CacheModule.register({ ttl: 0, max: 10 * 1000 * 1000 /* 10 MB */ })],
   providers: [ReferenceCacheService],
   exports: [ReferenceCacheService]
 })
