e57ea0f5aea7bc2852a1bcdc853995754ab112ec
Ruud van Buul <r.vanbuul@bedandbreakfast.eu>
2026-05-12 14:51:44 -0600

    - try changing dependencies

--- packages/query-graphql/src/auth/default-crud.authorizer.ts
+++ packages/query-graphql/src/auth/default-crud.authorizer.ts
@@ -35,7 +35,7 @@ export function createDefaultAuthorizer<DTO>(
     private readonly relations: Map<string, ResolverRelation<unknown>>
 
     constructor(
-      @Inject(ModuleRef) private readonly moduleRef: ModuleRef,
+      private readonly moduleRef: ModuleRef,
       @Optional() @Inject(getCustomAuthorizerToken(DTOClass)) private readonly customAuthorizer?: CustomAuthorizer<DTO>
     ) {
       this.relationsAuthorizers = new Map<string, Authorizer<unknown> | undefined>()
